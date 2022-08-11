c90eea7d9cff8c33e60b9826aa13575703d13d04
yunguks <walk1009@naver.com>
2022-08-11 18:31:44 +0900

    'update'

--- 10972_다음순열.py
+++ 10972_다음순열.py
@@ -35,30 +35,52 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 ans = list(map(int,input().split()))
-order = [i+1 for i in range(N)]
 
-def check(input,ans,c,N):
-    global order
-    if c ==-1:
-        return -1
+# 뒤에서 부터 i < i+1 일때 바꾼다.
+for i in range(N-2,-1,-1):
+    # 바뀌는 부분 i를 찾고 i보다 큰 수가 있다면 뒤에서 부터 있따면 바꾼다.
+    # 왜? i 부터 그후는 내림 차순 정렬 되 어 있을 것이므로
+    # 뒤에서 부터 큰 것이 나올 때 바꾼다면 , i수 다음 번 으로 큰 수 일 것이기 때문 
+    if ans[i] < ans[i+1]:
+        for j in range(N-1,i,-1):
+            if ans[i] < ans[j]:
+                ans[i],ans[j] = ans[j], ans[i]
+                # 바꾸고 나머지는 정렬
+                ans[i+1:] = sorted(ans[i+1:])
+                print(*ans)
+                exit()
+print(-1)
 
-    if input == ans:
-        print(input,ans)
-        c =1
-        return c
-    if len(input) <N:
+'''
+틀린 풀이
+check = -1
+# 맨 뒤 수보다 작은 수가 나올 때 까지
+for i in range(N-2,-1,-1):
+    if ans[i] < ans[-1]:
+        check = i
+        break
+# 맨 뒤의 수보다 작은 것도 나을 수 있으나,
+# 
+# 맨 뒤보다 작은 수 그 뒤 수에서 비교한다.
+# 모든 수가 내림차순이라면 맨 뒤와 check를 바꾸고
+# 아니라면 오름 차순이 되는 순간의 수랑 바꾼다.
+temp = check
+for i in range(N-2,check,-1):
+    if ans[i] < ans[i+1]:
+        temp= i
+        break
 
-        for i in range(N):
-            if order[i] not in input:
-                input.append(order[i])
-                if c==1 and len(input)==N:
-                    print(*input)
-                    return -1
-                c = check(input,ans,c,N)
-                input.pop()
-    return c
-if ans == sorted(order,reverse=True):
+if temp==-1:
     print(-1)
-else:
-    check([],ans,0,N)
+    exit()
 
+elif temp==check:
+    ans[check], ans[-1] = ans[-1], ans[check]
+else:
+    ans[temp], ans[temp+1] = ans[temp+1], ans[temp]
+
+# 작은 수가 나온 i-1번 째 수랑 i번쨰 수랑 바꾸고
+# 나머지 수는 정렬한다.
+ans[temp+1:]=sorted(ans[temp+1:])
+print(*ans) 
+'''
